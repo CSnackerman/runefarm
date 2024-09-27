217ad2fd644c5ede24de795bf6ef761eefefea95
ca <colbyackerman@gmail.com>
2024-09-26 23:17:40 -0500

    first

---
+++ main.py
@@ -0,0 +1,24 @@
+import keyboard
+import time
+
+ash_of_war_key = '9'
+
+def press_release(key: str, hold_seconds: int = 0.05):
+  keyboard.press(key)
+  time.sleep(hold_seconds)
+  keyboard.release(key)
+
+while 1:
+  time.sleep(8)
+  press_release('w', 2.5)
+  press_release('a', 0.7)
+  press_release('w', 2.5)
+  press_release(ash_of_war_key)
+  time.sleep(5)
+  press_release('g')
+  time.sleep(1)
+  press_release('f')
+  time.sleep(0.5)
+  press_release('e')
+  time.sleep(1)
+  press_release('e')
