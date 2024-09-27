a2fefce13d81c1e68f0c0dbf12d0762ca4a0d5f4
ca <colbyackerman@gmail.com>
2024-09-27 00:40:02 -0500

    GRACEful exit

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ def press_release(key: str, hold_seconds: float = 0.05):
 
 print('farming runes...')
 
-while 1:
-  time.sleep(7)
-  press_release('w', 2.5)
-  press_release('a', 0.7)
-  press_release('w', 2.5)
-  press_release(ash_of_war_key)
-  time.sleep(5)
-  press_release('g')
-  time.sleep(1)
-  press_release('f')
-  time.sleep(0.5)
-  press_release('e')
-  time.sleep(1)
-  press_release('e')
+try:
+  while 1:
+    time.sleep(7)
+    press_release('w', 2.5)
+    press_release('a', 0.7)
+    press_release('w', 2.5)
+    press_release(ash_of_war_key)
+    time.sleep(5)
+    press_release('g')
+    time.sleep(1)
+    press_release('f')
+    time.sleep(0.5)
+    press_release('e')
+    time.sleep(1)
+    press_release('e')
+except KeyboardInterrupt:
+  print('the light has faded; it is time to rest.')
+  exit(0)
